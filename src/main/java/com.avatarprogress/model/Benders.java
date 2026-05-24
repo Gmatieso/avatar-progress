ca2fd722b9b9fa16f6f73f6a00ccef91a05ffd74
builtbyGm <matiesogeoffrey@gmail.com>
2026-05-24 22:12:27 +0300

    Fix: Create an instance of Aaang and Katara

---
+++ src/main/java/com.avatarprogress/model/Benders.java
@@ -0,0 +1,25 @@
+package com.avatarprogress.model;
+
+import java.awt.*;
+
+public class Benders {
+    public static final Bender AANG = new Bender(
+            "Aaang",
+            Element.AIR,
+            "/sprite/aang/walk.gif",
+            new Color(0xE65100),
+            new Color(0xFFCC80)
+    );
+
+
+    public static final Bender KATARA = new Bender(
+            "Katara",
+            Element.WATER,
+            "/sprite/katara/walk.gif",
+            new Color(0x1565C0),
+            new Color(0x90CAF9)
+    );
+
+
+    }
+
