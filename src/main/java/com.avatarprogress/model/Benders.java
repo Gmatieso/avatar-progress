3e9cad5eab91020956cd10a73e497eac2a54cc6b
builtbyGm <matiesogeoffrey@gmail.com>
2026-05-24 22:24:07 +0300

    Fix: Create an instance of Toph and Zuko

--- src/main/java/com.avatarprogress/model/Benders.java
+++ src/main/java/com.avatarprogress/model/Benders.java
@@ -1,6 +1,7 @@
 package com.avatarprogress.model;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Benders {
     public static final Bender AANG = new Bender(
@@ -19,6 +20,24 @@ public class Benders {
             new Color(0x1565C0),
             new Color(0x90CAF9)
     );
+
+    public static  final Bender TOPH = new Bender(
+            "Toph",
+            Element.EARTH,
+                "/sprite/toph/walk.gif",
+            new Color(0x2E7D32),
+            new Color(0xA5D6A7)
+    );
+
+    public static final Bender ZUKO  = new Bender(
+            "ZUKO",
+            Element.FIRE,
+            "/sprite/zuko/walk.gif",
+            new Color(0xB71C1C),
+            new Color(0xFF8A65)
+    );
+
+    public static final List<Bender> ALL = new ArrayList<>();
 
 
     }
