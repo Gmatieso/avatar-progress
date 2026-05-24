539ed9b5ca5206ff97c36639b95216b4ae4c6363
builtbyGm <matiesogeoffrey@gmail.com>
2026-05-24 20:27:55 +0300

    Chore: Modeling our bender class

---
+++ src/main/java/com.avatarprogress/model/Bender.java
@@ -0,0 +1,43 @@
+package com.avatarprogress.model;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Bender {
+
+    private  final String name;
+    private  final Element element;
+    private final String spritePath;
+    private final Color primaryColor;
+    private final Color secondaryColor;
+    private  Icon cachedIcon;
+
+    public Bender(String name, Element element, String spritePath, Color primaryColor, Color secondaryColor, Icon cachedIcon) {
+        this.name = name;
+        this.element = element;
+        this.spritePath = spritePath;
+        this.primaryColor = primaryColor;
+        this.secondaryColor = secondaryColor;
+        this.cachedIcon = cachedIcon;
+    }
+
+    public String getName() {return name;}
+    public Element getElement() {return element;}
+    public Color getPrimaryColor() {return primaryColor;}
+    public Color getSecondaryColor() {return secondaryColor;}
+
+
+    public Icon getIcon() {
+        if(cachedIcon == null) {
+            java.net.URL url = Bender.class.getResource(spritePath);
+            if(url != null) {
+                cachedIcon = new ImageIcon(url);
+            }
+        }
+        return cachedIcon;
+    }
+
+    public String getTooltip(){
+        return  name + " - " + element.name().toLowerCase() + "bender";
+    }
+}
