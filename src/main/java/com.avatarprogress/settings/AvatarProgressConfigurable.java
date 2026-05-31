9e267a035c14b941e08d275134dddce23a503c72
builtbyGm <matiesogeoffrey@gmail.com>
2026-05-31 15:46:52 +0300

    Fix: Add your settings panel under Appearance & Behavior

--- src/main/java/com.avatarprogress/settings/AvatarProgressConfigurable.java
+++ src/main/java/com.avatarprogress/settings/AvatarProgressConfigurable.java
@@ -1,6 +1,30 @@
 package com.avatarprogress.settings;
 
 import com.intellij.openapi.options.Configurable;
+import com.intellij.openapi.options.ConfigurationException;
+import com.intellij.openapi.util.NlsContexts;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
 
 public class AvatarProgressConfigurable implements Configurable {
+    @Override
+    public @NlsContexts.ConfigurableName String getDisplayName() {
+        return "";
+    }
+
+    @Override
+    public @Nullable JComponent createComponent() {
+        return null;
+    }
+
+    @Override
+    public boolean isModified() {
+        return false;
+    }
+
+    @Override
+    public void apply() throws ConfigurationException {
+
+    }
 }
